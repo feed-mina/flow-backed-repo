a726f06f91bdc350a12abfe5b15038ecf6e8e008
feed-mina <myelin24@naver.com>
2025-07-20 21:30:04 +0900

    TypeORM 설정

--- flow-blocker-backend/models/index.js
+++ flow-blocker-backend/models/index.js
@@ -1,12 +1,17 @@
 const sequelize = require('../config/db');
+const config = require('../config/config.json')[process.env.NODE_ENV || 'development'];
 const BlockedExtension = require('./BlockedExtension');
 const UploadLog = require('./UploadLog');
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 BlockedExtension.initModel(sequelize);
 UploadLog.initModel(sequelize);
 
-module.exports = {
+
+const db = {
   sequelize,
   BlockedExtension,
   UploadLog,
 };
+
+module.exports = db;
